b6969802348684a3f9b378b565c2ea91e15c25a5
moody415 <mahmudul4155@gmail.com>
2022-08-01 14:46:59 -0400

    JS

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
-console.log('Hello World' [10])
+let fullName = 'Mahmudul Hasan'
+let isRaining = true
+let temperature = 20
+
+temperature = temperature + 2
+
+console.log(temperature)
+
+ let subscribed = true
+
+ if (subscribed ===true) {
+     console.log ('show the video')
+
+ }
+ else {
+     console.log('tell user to log into account')
+ }
+
+
