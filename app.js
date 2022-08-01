7cb1d69fed824d0c01491e48887b7ebe2fb9b349
moody415 <mahmudul4155@gmail.com>
2022-08-01 19:18:16 -0400

    JS

--- app.js
+++ app.js
@@ -1,64 +1,15 @@
-let fullName = 'Mahmudul Hasan'
-let isRaining = true
-let temperature = 20
 
-temperature = temperature + 2
-
-console.log(temperature)
-
-
- // if else statement examples
-
-
- let subscribed = true
- let loggedIn = true
-
- if (subscribed === true) {
-     console.log ('show the video')
-
- }
- else if (loggedIn === true) {
-     console.log('tell the user to upgrade')
- }
- else {
-     console.log('tell user to log into account')
- }
-
- let cash = 50
- let cost = 40
-
- if (cash>cost) {
-     console.log("successful transaction")
- }
- else if (cash<cost)  {
-     console.log("Unsuccessful transaction due to insufficient funds")
- }
- else{
-     console.log("Have a great day.")
- }
-
-
- let hot = true
-
-hot ? console.log('weather is hot outside')  : console.log('weather is cold')
-
-//
-
-let currency = 50
-let price = 40
-let isStoreOpen = true
-
-cash>=price && isStoreOpen ? console.log("give receipt") : console.log("do not give receipt")
-
-
-let count = 1
-
-while (count<=100) {
-    console.log(count)
-    count++
-}
-
-for (let i = 0; i<3; i++) {
+for (i=1; 1<=20; i++) {
     console.log(i)
+
+if (i / 5 === 0) {
+    console.log("FSimplified")
+}   
+ else (i / 3 === 0); {
+    console.log("Frontend") 
+}
+if (i % 3 && i % 5 ); {
+    console.log("Frontend Simplified")
+}
 
 }
--- fes.js
+++ fes.js
@@ -2,3 +2,14 @@ for (i=1; 1<=20; i++) {
     console.log(i)
 }
 
+let number = 5
+
+if (number % 5) {
+    console.log("Frontend")
+    
+} else {
+    
+    
+}
+
+
