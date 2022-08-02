a4c8c9b71d8e7957c1be7888d62bee3c86701db0
moody415 <mahmudul4155@gmail.com>
2022-08-02 12:10:47 -0400

    JS

--- app.js
+++ app.js
@@ -68,5 +68,15 @@ arr.push(200)
 console.log(arr)
 */
 
-arr.filter((element) =>  {} )
- 
+let arr = [20, 30, 40, 50, 100]
+
+let newArr = arr.filter((element) =>  {
+    console.log(element)
+    if (element<50) {
+        return true;
+    }
+
+} 
+) 
+
+console.log(newArr)
