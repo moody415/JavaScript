d07b4180345c294ee51ea8bc1dc115982ee37703
moody415 <mahmudul4155@gmail.com>
2022-08-01 21:06:30 -0400

    JS

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+// DRY - Don't Repeat Yourself
 for (let i=1; i<=20; i++) {
 if (i % 3 === 0 && i % 5 === 0) {
     console.log(`${i} -> Frontend Simplified`)
@@ -21,3 +21,8 @@ for (let i=0; i<str.length; i++) {
     console.log(str[i])
 
 }
+
+function welcomePersonFes() {
+    
+}
+
