cebf1c994972ce2e5dafec22d0c014eaccb757f4
moody415 <mahmudul4155@gmail.com>
2022-08-01 20:59:44 -0400

    JS

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 
-for (i=1; 1<=20; i++) {
-    console.log(i)
+for (let i=1; i<=20; i++) {
+if (i % 3 === 0 && i % 5 === 0) {
+    console.log(`${i} -> Frontend Simplified`)
+}
+else if (i % 3 === 0) {
+console.log(`${i} -> Frontend`)
+}
+else if (i % 5 === 0) {
+    console.log(`${i} -> Simplified`)
+}
 
-if (i / 5 === 0) {
-    console.log("FSimplified")
-}   
- else (i / 3 === 0); {
-    console.log("Frontend") 
+else {
+    console.log(`${i} -> ${i}`)
 }
-if (i % 3 && i % 5 ); {
-    console.log("Frontend Simplified")
 }
+
+const str = "Frontend Simplified"
+
+for (let i=0; i<str.length; i++) {
+    console.log(str[i])
 
 }
